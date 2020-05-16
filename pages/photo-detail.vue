6818587db86f55b910d4414f3ee8865445f1737c
rc <rc@hudl.us>
2020-05-16 10:10:35 -0700

    now with v-chips on detail page

--- pages/photo-detail.vue
+++ pages/photo-detail.vue
@@ -1,16 +1,31 @@
 <template>
     <v-container
-       class="fill-height"
+       max-width="1000"
         fluid>
         <v-row>
             <v-col>
-                <v-card
-                    height="450">
+                <v-card 
+                    class="mx-auto"
+                    height="450"
+                    max-width="450">
                     <v-img
                         contain="true"
                         height="550"
-                        src="https://res.cloudinary.com/louise/image/upload/h_450/slides--86.jpg"
+                        src="https://res.cloudinary.com/louise/image/upload/slides--86.jpg"
                     />
+                    <v-card-actions>
+                        <v-chip
+                        class="ma-2"
+                        color="indigo"
+                        text-color="white"
+                        >
+                        <v-avatar left>
+                            <v-icon>mdi-account-circle</v-icon>
+                        </v-avatar>
+                        louise
+                        </v-chip>
+                        <v-chip>travel</v-chip>
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
