c2ce2bc80fb6a8dd8aac69c3f0cc6479054f83b5
rc <rc@hudl.us>
2020-05-28 07:27:17 -0700

    some responsive  tweaks and adding calendar invites

---
+++ pages/service.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-card max-width="1500" class="mx-auto">
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <v-card>
+              <h2>Funeral Service and Events</h2>
+            </v-card>
+            <v-card>
+            </v-card>
+          </v-col>
+          <v-col 
+          cols="12"
+          sm="6"
+          lg="4">
+            <v-card>
+              <v-carousel
+                cycle
+                height="400"
+                hide-delimiter-background
+                show-arrows-on-hover
+              >
+                <v-carousel-item
+                  v-for="(slide, i) in slides"
+                  :key="i"
+                >
+                  <v-sheet
+                    :color="colors[i]"
+                    height="100%"
+                  >
+                    <v-row
+                      class="fill-height"
+                      align="center"
+                      justify="center"
+                    >
+                      <div class="display-3">{{ slide }} Slide</div>
+                    </v-row>
+                  </v-sheet>
+                </v-carousel-item>
+              </v-carousel>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+      }
+    },
+  }
+</script>
