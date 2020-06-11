88d7ace66dcfd07b6c6ffc241069a541bdb29753
costibotez <costibotez94@gmail.com>
2020-06-10 22:30:04 -0400

    fix role issue

--- server/role.js
+++ server/role.js
@@ -6,12 +6,7 @@ const clientSecret = process.env.AUTH_SECRET
 const auth_domain = process.env.AUTH_DOMAIN
 
 export default async function (req, res, next) {
-  const email = req.query.email;
-  if (!email) {
-    console.log("No email provided to get roles");
-    res.json([]);
-    return;
-  } 
+  const id = req.query.id;
   try{
     console.log("Trying to get token for auth0");
     const { data: { access_token, token_type } } = await axios.post(`https://${auth_domain}/oauth/token`, {
@@ -23,36 +18,19 @@ export default async function (req, res, next) {
     if (access_token)
       console.log("Received Token");
     try{
-      console.log("Trying to find user by email: ", email);
-      const { data: users } = await axios.get(`https://${auth_domain}/api/v2/users?q=email:"${email}"&search_engine=v3`, {
-        headers: {
-          "Authorization": `${token_type} ${access_token}`
-        }
-      })
-      if (users[0]) {
-        console.log("Found user: ", users[0].user_id);
-        try{
-          console.log("Trying to get roles");
-          const { data: roles } = await axios.get(`https://${auth_domain}/api/v2/users/${users[0].user_id}/roles`, {
-            headers: {
-              "Authorization": `${token_type} ${access_token}`
-            }
-          })
-          console.log("Received roles: ", roles);
-          res.json(roles);
-        } catch(err){
-          console.log(err)
-          next(err)
-        }
-      } else {
-        console.log("Couldn't find user!");
-        next("Couldn't find user!");
+        console.log("Trying to get roles");
+        const { data: roles } = await axios.get(`https://${auth_domain}/api/v2/users/${id}/roles`, {
+          headers: {
+            "Authorization": `${token_type} ${access_token}`
+          }
+        })
+        console.log("Received roles: ", roles);
+        res.json(roles);
+      } catch(err){
+        console.log(err)
+        next(err)
       }
-    }catch(err){
-      console.log(err)
-      next(err)
-    }
-
+      
   }catch(err){
     console.log(err)
     next(err)
