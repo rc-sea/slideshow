d471d5c96c1c69efea671deab3615aaebcff5d0f
Jacek Karczmarczyk <jk@drukcd.pl>
2020-07-16 23:40:00 +0200

    load min 48 untagged photos

--- server/search.js
+++ server/search.js
@@ -1,15 +1,11 @@
+import e from 'express';
+
 const cloudinary = require('./cloudinary').cloudinary;
 
-export default async function (req, res, next) {
-  const searchtag = req.query.searchtag;
-  const type = req.query.type | 0;
-  const next_cursor = req.query.next_cursor;
+const LIMIT = 48;
 
+async function fetchPhotos (searchtag, type, next_cursor) {
   let query;
-
-  console.log('SearchTag: ', searchtag);
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   if (searchtag) {
     const expression = searchtag === '-'
@@ -19,7 +15,7 @@ export default async function (req, res, next) {
     query = cloudinary.search
       .expression(expression)
       .next_cursor(next_cursor)
-      .max_results(48);
+      .max_results(LIMIT);
 
     if (searchtag === '-') query = query.with_field('tags');
   } else {
@@ -32,5 +28,28 @@ export default async function (req, res, next) {
     result.resources = result.resources.filter(image => !image.tags.length);
   }
 
+  return result;
+}
+
+export default async function (req, res, next) {
+  const searchtag = req.query.searchtag;
+  const type = req.query.type | 0;
+
+  let next_cursor = req.query.next_cursor;
+
+  let result = null;
+
+  do {
+    const localResult = await fetchPhotos(searchtag, type, next_cursor);
+
+    if (result) {
+      localResult.resources = result.resources.concat(localResult.resources);
+    }
+    result = localResult;
+    next_cursor = result.next_cursor;
+  } while (searchtag === '-' && next_cursor && result.resources.length <= LIMIT); // eslint-disable-line no-unmodified-loop-condition
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.json(result);
 }
