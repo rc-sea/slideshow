5a6bda61db1fe6fe257e76692410dadd48b1ac25
Jacek Karczmarczyk <jk@drukcd.pl>
2020-07-18 20:29:10 +0200

    tweaks
    fixes #72

---
+++ components/CardActions.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-divider color="#444" />
+    <v-card-actions class="pa-3" style="background: #333">
+      <v-spacer />
+      <v-btn
+        :block="main"
+        :color="main ? 'primary' : ''"
+        :depressed="main"
+        :disabled="disabled"
+        :large="main"
+        :loading="loading"
+        :rounded="main"
+        :text="!main"
+        @click="$emit('click')"
+      >
+        <v-icon v-if="icon" left small>{{ icon }}</v-icon>
+        {{ label }}
+      </v-btn>
+    </v-card-actions>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardActions',
+
+  props: {
+    disabled: Boolean,
+    icon: String,
+    label: {
+      type: String,
+      required: true,
+    },
+    loading: Boolean,
+    main: Boolean,
+  },
+};
+</script>
